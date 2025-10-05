0451240a646d5304f76fdf3afa21a85282666d4c
“twinkleaP” <twinklekumari12@gmail.com>
2025-10-04 22:21:04 -0400

    added changes to main.go to include metrics collection and sending to server

--- go-service/main.go
+++ go-service/main.go
@@ -5,37 +5,12 @@ import (
 	"go-service/client"
 	"go-service/metrics"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Metrics struct {
 	CPU    float64 `json:"cpu"`
 	Memory float64 `json:"memory"`
 	Disk   float64 `json:"disk"`
-}
-
-var (
-	cpuGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "system_cpu_percent",
-		Help: "CPU usage percent",
-	})
-	memoryGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "system_memory_percent",
-		Help: "Memory usage percent",
-	})
-	diskGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "system_disk_percent",
-		Help: "Disk usage percent",
-	})
-	anomalyCounter = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "anomalies_detected_total",
-		Help: "Total anomalies detected",
-	})
-)
-
-func init() {
-	prometheus.MustRegister(cpuGauge, memoryGauge, diskGauge, anomalyCounter)
 }
 
 func main() {
